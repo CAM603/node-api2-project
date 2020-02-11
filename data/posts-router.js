6e29aabe0f56d505b6a457ec1833c457db44d81b
CAM603 <cameronhawley@comcast.net>
2020-02-11 17:47:00 -0500

    ADD get comments for post

--- data/posts-router.js
+++ data/posts-router.js
@@ -15,6 +15,47 @@ router.get('/', (req, res) => {
         })
 })
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.findById(id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json(post)
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params;
+
+    Posts.findById(id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                Posts.findPostComments(id)
+                    .then(comments => {
+                        res.status(200).json(comments)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).json({ error: "The comments information could not be retrieved." })
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 router.post('/', (req, res) => {
     const postInfo = req.body;
 
@@ -30,6 +71,34 @@ router.post('/', (req, res) => {
                     error: "There was an error while saving the post to the database" })
             })
     }
+})
+
+// Needs checking
+router.post('/:id/comments', (req, res) => {
+    const commentInfo = req.body;
+    const { id } = req.params;
+
+    Posts.findById(id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else if (!commentInfo.text) {
+                res.status(400).json({ errorMessage: "Please provide text for the comment." })
+            } else {
+                Posts.insertComment(req.body)
+                    .then(comment => {
+                        res.status(201).json(comment)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).json({ error: "There was an error while saving the comment to the database" })
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
 
 })
 
