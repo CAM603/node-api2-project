21a919143358c388ae6bc83c9bf74b5c7955a3f1
CAM603 <cameronhawley@comcast.net>
2020-02-11 18:19:11 -0500

    ADD delete request

--- data/posts-router.js
+++ data/posts-router.js
@@ -101,4 +101,28 @@ router.post('/:id/comments', (req, res) => {
 
 })
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Posts.findById(id)
+        .then(post => {
+            if(post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                Posts.remove(id)
+                    .then(removedPost => {
+                        res.status(200).json(post)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).json({ error: "The post could not be removed" })
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+})
+
 module.exports = router;
