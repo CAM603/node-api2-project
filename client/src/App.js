b7dd966d261a7ce9793a28b3ad7d8f0f11467d14
CAM603 <cameronhawley@comcast.net>
2020-02-11 21:54:53 -0500

    ADD input form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios'
+import Comments from './Comments';
+import AddPost from './AddPost';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = () => {
+    axios.get(`http://localhost:5000/api/posts`)
+      .then(res => {
+        console.log(res)
+        setPosts(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  const addPost = (post) => {
+    axios.post(`http://localhost:5000/api/posts`, post)
+      .then(res => {
+        getPosts()
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Posts</h1>
+      <AddPost
+      addPost={addPost}
+      />
+      {posts.map(post => (
+        <div className="post-container" key={post.id}>
+          <div className="poster-container">
+            <span className="poster">👩🏽</span>
+            <h2>{post.title}</h2>
+            <h2>{post.contents}</h2>
+          </div>
+          
+            <Comments post={post}/>
+          
+        </div>
+      ))}
     </div>
   );
 }
